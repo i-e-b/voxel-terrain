0b30be588e9c56644dbd81516d315aaab81db8c3
i-e-b <ieb@me.com>
2017-10-13 22:33:07 +0100

    radical change to color lookup method.

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
 	t.window.width = 800
-	t.window.height = 600
+	t.window.height = 400
 
 	t.modules.joystick = false
 	t.modules.physics = false
--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ MIN_HEIGHT = 10
 MAX_PITCH = -200
 MIN_PITCH = 100
 
-VIEW_DISTANCE = 500 -- how far to draw. More is slower but you can see further (range: 400 to 2000)
+VIEW_DISTANCE = 400 -- how far to draw. More is slower but you can see further (range: 400 to 2000)
 
 PIXEL_SCALE = 2 -- Resolution reduction. Higher number is faster, lower is better quality
 PIXEL_WIDTH = love.graphics.getWidth() / PIXEL_SCALE
@@ -19,9 +19,10 @@ local interlace = 0
 
 local mapIndex = 1
 local ctrlLock = 0
-local map = {}
+local color_R, color_G, color_B = {},{},{}
+local sky_R, sky_G, sky_B = {},{},{}
 local heights = {}
-depth = love.graphics.getHeight() * 0.5 -- steepness of slopes. Lower numbers = taller mountains
+depth = love.graphics.getHeight() --* 0.5 -- steepness of slopes. Lower numbers = taller mountains
 
 camera = { -- init player camera
 	x = 512,
@@ -34,8 +35,8 @@ camera = { -- init player camera
 
 local mapData
 local heightData
-local mapWidth
-local mapHeight
+local mapWidth, mapHeight
+local skyWidth, skyHeight
 local height
 
 function rayCast(line, x1, y1, x2, y2, d, xDir)
@@ -44,7 +45,7 @@ function rayCast(line, x1, y1, x2, y2, d, xDir)
 	local dy = y2 - y1
 
 	-- distance between start and end point
-	local r = math.floor(math.sqrt(dx * dx + dy * dy))
+	local r = math.sqrt(dx * dx + dy * dy)
 
 	local dp = math.abs(d) / 100
 	local persp = 0
@@ -63,27 +64,27 @@ function rayCast(line, x1, y1, x2, y2, d, xDir)
 	local gap = 1 -- marks when we should break slope colour interpolation
 	local hbound = height - 1
 
-	local sw = sky:getWidth() - 1
-	local sh = sky:getHeight() - 1
-	local sx = -xDir*PIXEL_SCALE*(PIXEL_WIDTH / math.pi)
+	local sx = -xDir*PIXEL_SCALE*(PIXEL_WIDTH / math.pi) + line
 
 	-- fog parameters
 	local dlimit = VIEW_DISTANCE * 0.7
 	local dfog = 1 / (VIEW_DISTANCE * 0.3)
-	local fo = 0
-	local fs = 1
+	local fo,fs = 0,1
+
+	local x,y,idx
 
 	-- MAIN LOOP
 	-- we draw from near to far
-	for i = 1,VIEW_DISTANCE do
+	for i = 0,VIEW_DISTANCE do
 		-- step to next position, wrapped for out-of-bounds
 		x1 = math.abs((x1 + dx) % mapWidth)
 		y1 = math.abs((y1 + dy) % mapHeight)
 		x = math.floor(x1)
 		y = math.floor(y1)
+		idx = (x * mapWidth)+y
 
 		-- get height
-		h = camera.height - heights[x][y]
+		h = camera.height - heights[idx]  -- lack of interpolation here causes banding artifacts close up
 
 		-- perspective calculation where d is the correction parameter
 		persp = persp + dp
@@ -101,17 +102,14 @@ function rayCast(line, x1, y1, x2, y2, d, xDir)
 			local iz = math.min(hbound, ymin)
 
 			-- read color from image
-			local data = map[x][y]
-			local r = bit.rshift(bit.band(data, 0x00FF0000), 16)
-			local g = bit.rshift(bit.band(data, 0x0000FF00), 8)
-		  local b = bit.band(data, 0x000000FF)
+			local r,g,b = color_R[idx],color_G[idx],color_B[idx]
 
 			-- fog effect
 			if (doFog) and (i > dlimit) then -- near the fog limit
-				local sr,sg,sb = sky:getPixel((sx + line) % sw, ir % sh) -- read background behind
-				r = (r * fs) + (fo * sr)
-				g = (g * fs) + (fo * sg)
-				b = (b * fs) + (fo * sb)
+				idx = (math.floor(sx % skyWidth)*skyWidth) + (ir % skyHeight)
+				r = (r * fs) + (fo * sky_R[idx])
+				g = (g * fs) + (fo * sky_G[idx])
+				b = (b * fs) + (fo * sky_B[idx])
 				pr=r;pg=g;pb=b;
 			end
 
@@ -122,11 +120,13 @@ function rayCast(line, x1, y1, x2, y2, d, xDir)
 
 				-- Jitter samples to make smoothing look better (otherwise orthagonal directions look stripey)
 				if (doJitter) and (i < dlimit) then -- don't jitter if drawing fog
-					-- pull nearby sample to blend
-					data = map[math.ceil(x1+0.5)][math.ceil(y1+0.5)]
-					r = (r + bit.rshift(bit.band(data, 0x00FF0000), 16)) / 2
-					g = (g + bit.rshift(bit.band(data, 0x0000FF00), 8)) / 2
-				  b = (b + bit.band(data, 0x000000FF)) / 2 --]]
+					--[ pull nearby sample to blend
+					x = math.floor((x1+(dy/2))  % mapWidth)
+					y = math.floor((y1+(dx/2)) % mapHeight)
+					local jidx = (x*mapWidth)+y
+					r = (r + color_R[jidx]) / 2
+					g = (g + color_G[jidx]) / 2
+				  b = (b + color_B[jidx]) / 2 --]]
 				end
 
 				if (doSmoothing) then
@@ -138,17 +138,17 @@ function rayCast(line, x1, y1, x2, y2, d, xDir)
 						pr = pr + sr
 						pg = pg + sg
 						pb = pb + sb
-						imageData:setPixel(k, line, pr, pg, pb, 255)
+						imageData:setPixel(line, k, pr, pg, pb, 255)
 					end
 				else -- no smoothing, just fill in with sample color
 					for k = iz,ir,-1 do
-						imageData:setPixel(k, line, r, g, b, 255)
+						imageData:setPixel(line, k, r, g, b)
 					end
 				end
 
 			else -- small textels. Could supersample for quality?
 				pr=r;pg=g;pb=b; -- copy previous colors
-				imageData:setPixel(ir, line, r, g, b, 255)
+				imageData:setPixel(line, ir, r, g, b)
 			end
 			gap = 0
 		else
@@ -159,9 +159,9 @@ function rayCast(line, x1, y1, x2, y2, d, xDir)
 	end
 
 	-- now if we didn't get to the top of the screen, fill in with sky
-	for i=ymin,1,-1 do
-		local sr,sg,sb = sky:getPixel((sx + line) % sw, i % sh)
-		imageData:setPixel(i % height, line, sr,sg,sb)
+	for i=ymin,0,-1 do
+		idx = (math.floor(sx % skyWidth)*skyWidth) + (i % skyHeight)
+		imageData:setPixel(line, i % height, sky_R[idx],sky_G[idx],sky_B[idx])
 	end
 end
 
@@ -169,26 +169,33 @@ function loadMap(index)
 	-- combine height and color data
 	mapData = love.image.newImageData("maps/C" .. tostring(index) .. "W.png")
 	heightData = love.image.newImageData("maps/C" .. tostring(index) .. "D.png")
-	map = {}
+	--map = {}
+	color_R = {}
+	color_G = {}
+	color_B = {}
 	heights = {}
 	local iw = mapData:getWidth()
 	local ih = mapData:getHeight()
 	for x = 0,iw+1 do -- we overscan slightly to remove some bounds checks from the rayCast function
-		map[x] = {}
-		heights[x] = {}
+		--map[x] = {}
+		--heights[x] = {}
 		for y = 0,ih+1 do
 			local r,g,b = mapData:getPixel(x % iw, y % ih)
 			-- since the heightmap is black/white the r,g,b values of every pixel will be equal
 			-- and between 0 and 255 so we can just use the first red value to interpret the height
 			local h = heightData:getPixel(x % iw, y % ih)
+			local idx = (x*iw)+y
 			-- mapping to a single number, needs bitwise unpacking later
-			map[x][y] = bit.bor(bit.lshift(r, 16), bit.bor(bit.lshift(g, 8), b))
-			heights[x][y] = h
+			color_R[idx] = r
+			color_G[idx] = g
+			color_B[idx] = b
+			--map[idx] = bit.bor(bit.lshift(r, 16), bit.bor(bit.lshift(g, 8), b))
+			heights[idx] = h
 		end
 	end
 	mapWidth = mapData:getWidth()
 	mapHeight = mapData:getHeight()
-	height = imageData:getWidth()
+	height = imageData:getHeight()
 end
 
 function love.load()
@@ -197,7 +204,21 @@ function love.load()
 
 	hud = love.graphics.newImage("hud.png")
 	sky = love.image.newImageData("sky.png")
-	imageData = love.image.newImageData(PIXEL_HEIGHT, PIXEL_WIDTH)
+	imageData = love.image.newImageData(PIXEL_WIDTH, PIXEL_HEIGHT)
+
+
+	skyWidth = sky:getWidth()
+	skyHeight = sky:getHeight()
+	for x = 0,skyWidth+1 do
+		for y = 0,skyHeight+1 do
+			local idx = (x*skyWidth)+y
+			local r,g,b = sky:getPixel(x % skyWidth, y % skyHeight)
+
+			sky_R[idx] = r
+			sky_G[idx] = g
+			sky_B[idx] = b
+		end
+	end
 
 	loadMap(1)
 end
@@ -279,8 +300,8 @@ function love.draw()
 	local cosAngle = math.cos(camera.angle)
 
 	local y3d = -depth * 1.5
-	for i = interlace,imageData:getHeight()-1,2 do
-		local x3d = (i - imageData:getHeight() / 2) * 1.5 * 1.5
+	for i = interlace,imageData:getWidth()-1,2 do -- increment by 2 for interlacing
+		local x3d = (i - imageData:getWidth() / 2) * 2.25
 
 		local rotX =  cosAngle * x3d + sinAngle * y3d
 		local rotY = -sinAngle * x3d + cosAngle * y3d
@@ -288,14 +309,13 @@ function love.draw()
 		rayCast(i, camera.x, camera.y, camera.x + rotX, camera.y + rotY, y3d / math.sqrt(x3d * x3d + y3d * y3d), camera.angle)
 	end
 
+	-- alternate scanlines each frame
 	interlace = 1 - interlace
 
 	if not bufferImage then bufferImage = love.graphics.newImage(imageData)
 	else bufferImage:refresh() end
 
-	love.graphics.draw(bufferImage, 0, 0, math.pi / 2,
-	(512 / bufferImage:getWidth()),
-	-(love.graphics.getWidth() / bufferImage:getHeight()))
+	love.graphics.draw(bufferImage, 0, 0, 0, PIXEL_SCALE)
 
 	-- draw hud and altimeter
 	--love.graphics.draw(hud, 0, 0)
