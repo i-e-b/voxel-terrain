85bf3902823fa507c1b8c4efaad81c60230a0309
i-e-b <ieb@me.com>
2017-10-16 21:47:13 +0100

    more optimisation

--- main.lua
+++ main.lua
@@ -1,16 +1,13 @@
--- Copyright (C) 2017 Robert B. Colton
--- The Pennsylvania State University
+local MAX_HEIGHT = 400
+local MIN_HEIGHT = 10
+local MAX_PITCH = -200
+local MIN_PITCH = 150
 
-MAX_HEIGHT = 400
-MIN_HEIGHT = 10
-MAX_PITCH = -200
-MIN_PITCH = 100
+local VIEW_DISTANCE = 600 -- how far to draw. More is slower but you can see further (range: 400 to 2000)
 
-VIEW_DISTANCE = 400 -- how far to draw. More is slower but you can see further (range: 400 to 2000)
-
-PIXEL_SCALE = 2 -- Resolution reduction. Higher number is faster, lower is better quality
-PIXEL_WIDTH = love.graphics.getWidth() / PIXEL_SCALE
-PIXEL_HEIGHT = love.graphics.getHeight() / PIXEL_SCALE
+local PIXEL_SCALE = 3 -- Resolution reduction. Higher number is faster, lower is better quality
+local PIXEL_WIDTH = love.graphics.getWidth() / PIXEL_SCALE
+local PIXEL_HEIGHT = love.graphics.getHeight() / PIXEL_SCALE
 
 local doJitter = true
 local doFog = true
@@ -22,14 +19,14 @@ local ctrlLock = 0
 local color_R, color_G, color_B = {},{},{}
 local sky_R, sky_G, sky_B = {},{},{}
 local heights = {}
-depth = love.graphics.getHeight() --* 0.5 -- steepness of slopes. Lower numbers = taller mountains
+local depth = love.graphics.getHeight() -- steepness of slopes. Lower numbers = taller mountains
 
-camera = { -- init player camera
+local camera = { -- init player camera
 	x = 512,
 	y = 800,
 	height = 100,
 	angle = 0,
-	v = -100
+	v = 0
 }
 
 
@@ -44,101 +41,107 @@ function rayCast(line, x1, y1, x2, y2, d, xDir)
 	local dx = x2 - x1
 	local dy = y2 - y1
 
-	-- distance between start and end point
-	local r = math.sqrt(dx * dx + dy * dy)
 
 	local dp = math.abs(d) / 100
 	local persp = 0
 
 	-- calculate stepsize in x and y direction
-	dx = dx / r
-	dy = dy / r
+	local dr = math.sqrt(dx * dx + dy * dy) -- distance between start and end point
+	dx = dx / dr
+	dy = dy / dr
 
 	local ymin = height -- last place we ended drawing a vertical line
 										  -- also the highest thing we've drawn to prevent over-paint (0 is max height)
 	local z3 = ymin+1     -- projected Z height of point under consideration
 	local data, h
-	local pr=0
-	local pg=0
-	local pb=0
+	local pr,pg,pb=0,0,0
 	local gap = 1 -- marks when we should break slope colour interpolation
 	local hbound = height - 1
 
-	local sx = -xDir*PIXEL_SCALE*(PIXEL_WIDTH / math.pi) + line
+	local sx = math.floor( (-xDir*PIXEL_SCALE*(PIXEL_WIDTH / math.pi) + line) % skyWidth)*skyWidth
 
 	-- fog parameters
 	local dlimit = VIEW_DISTANCE * 0.7
 	local dfog = 1 / (VIEW_DISTANCE * 0.3)
 	local fo,fs = 0,1
+	local lastI = 0
 
 	local x,y,idx
 
+	-- local references to speed up the core loop: (this is surprisingly effective)
+	local abs = math.abs
+	local floor = math.floor
+	local min = math.min
+	local max = math.max
+	local camHeight = camera.height
+	local camV = camera.v
+
 	-- MAIN LOOP
 	-- we draw from near to far
+	-- first we do a tight loop through the height map looking for a position
+	-- that would be visible behind anything else we've drawn
+	-- then we loop through the pixels to draw and color them
 	for i = 0,VIEW_DISTANCE do
 		-- step to next position, wrapped for out-of-bounds
-		x1 = math.abs((x1 + dx) % mapWidth)
-		y1 = math.abs((y1 + dy) % mapHeight)
-		x = math.floor(x1)
-		y = math.floor(y1)
-		idx = (x * mapWidth)+y
+		x1 = abs((x1 + dx) % mapWidth)
+		y1 = abs((y1 + dy) % mapHeight)
+		x = floor(x1)
+		y = floor(y1)
 
 		-- get height
-		h = camera.height - heights[idx]  -- lack of interpolation here causes banding artifacts close up
+		idx = (x * mapWidth) + y
+		h = camHeight - heights[idx]  -- lack of interpolation here causes banding artifacts close up
 
 		-- perspective calculation where d is the correction parameter
 		persp = persp + dp
-		z3 = math.floor(h / persp - camera.v)
+		z3 = h / persp - camV
 
+		-- is this position is visible?
+		if (z3 < ymin) then -- (if you wanted to mark visible/invisible positions you could do it here)
+			z3 = floor(z3) -- get on to pixel bounds
 
-		if (i > dlimit) then -- near the fog limit
-			fs = fs - dfog -- advance the fog blend by distance
-			fo = fo + dfog
-		end
-
-		if (z3 < ymin) then -- this position is visible (if you wanted to mark visible/invisible positions you could do it here)
-			-- write verical strip, limited to buffer bounds
-			local ir = math.min(hbound, math.max(0,z3))
-			local iz = math.min(hbound, ymin)
+			-- bounds of vertical strip, limited to buffer bounds
+			local ir = min(hbound, max(0,z3))
+			local iz = min(hbound, ymin)
 
 			-- read color from image
 			local r,g,b = color_R[idx],color_G[idx],color_B[idx]
 
 			-- fog effect
 			if (doFog) and (i > dlimit) then -- near the fog limit
-				idx = (math.floor(sx % skyWidth)*skyWidth) + (ir % skyHeight)
+				fo = dfog*(i-dlimit) -- calculate the fog blend by distance
+				fs = 1 - fo
+				idx = (sx) + (ir % skyHeight)
 				r = (r * fs) + (fo * sky_R[idx])
 				g = (g * fs) + (fo * sky_G[idx])
 				b = (b * fs) + (fo * sky_B[idx])
-				pr=r;pg=g;pb=b;
 			end
 
-
-			if (gap > 0) then pr=r;pg=g;pb=b; end -- no prev colors
 			if (ir+1 < iz) then -- large textels, interpolate for smoothness
 				-- get the next color, interpolate between that and the previous
 
 				-- Jitter samples to make smoothing look better (otherwise orthagonal directions look stripey)
 				if (doJitter) and (i < dlimit) then -- don't jitter if drawing fog
-					--[ pull nearby sample to blend
-					x = math.floor((x1+(dy/2))  % mapWidth)
-					y = math.floor((y1+(dx/2)) % mapHeight)
+					-- pull nearby sample to blend
+					x = floor((x1+(dy/2))  % mapWidth)
+					y = floor((y1+(dx/2)) % mapHeight)
 					local jidx = (x*mapWidth)+y
 					r = (r + color_R[jidx]) / 2
 					g = (g + color_G[jidx]) / 2
-				  b = (b + color_B[jidx]) / 2 --]]
+				  b = (b + color_B[jidx]) / 2
 				end
 
 				if (doSmoothing) then
-					local pc = (iz - ir) * 1.5 -- slight bleed between samples
+					if (gap > 0) then pr=r;pg=g;pb=b; end -- no prev colors
+					local pc = (iz - ir) + 1
 					local sr = (r - pr)/pc
 					local sg = (g - pg)/pc
 					local sb = (b - pb)/pc
 					for k = iz,ir,-1 do
+						imageData:setPixel(line, k, pr, pg, pb)
 						pr = pr + sr
 						pg = pg + sg
 						pb = pb + sb
-						imageData:setPixel(line, k, pr, pg, pb, 255)
 					end
 				else -- no smoothing, just fill in with sample color
 					for k = iz,ir,-1 do
@@ -154,42 +157,38 @@ function rayCast(line, x1, y1, x2, y2, d, xDir)
 		else
 			gap = 1
 		end
-		ymin = math.min(ymin, math.floor(z3))
+		ymin = min(ymin, z3)
 		if (ymin < 1) then return end -- early exit: the screen is full
 	end
 
 	-- now if we didn't get to the top of the screen, fill in with sky
 	for i=ymin,0,-1 do
-		idx = (math.floor(sx % skyWidth)*skyWidth) + (i % skyHeight)
+		idx = (sx) + (i % skyHeight)
 		imageData:setPixel(line, i % height, sky_R[idx],sky_G[idx],sky_B[idx])
 	end
 end
 
 function loadMap(index)
-	-- combine height and color data
+	-- extract height and color data
 	mapData = love.image.newImageData("maps/C" .. tostring(index) .. "W.png")
 	heightData = love.image.newImageData("maps/C" .. tostring(index) .. "D.png")
-	--map = {}
+
 	color_R = {}
 	color_G = {}
 	color_B = {}
-	heights = {}
+	heights = {0,0,0,0}
 	local iw = mapData:getWidth()
 	local ih = mapData:getHeight()
 	for x = 0,iw+1 do -- we overscan slightly to remove some bounds checks from the rayCast function
-		--map[x] = {}
-		--heights[x] = {}
 		for y = 0,ih+1 do
-			local r,g,b = mapData:getPixel(x % iw, y % ih)
-			-- since the heightmap is black/white the r,g,b values of every pixel will be equal
-			-- and between 0 and 255 so we can just use the first red value to interpret the height
-			local h = heightData:getPixel(x % iw, y % ih)
 			local idx = (x*iw)+y
-			-- mapping to a single number, needs bitwise unpacking later
+
+			local r,g,b = mapData:getPixel(x % iw, y % ih)
 			color_R[idx] = r
 			color_G[idx] = g
 			color_B[idx] = b
-			--map[idx] = bit.bor(bit.lshift(r, 16), bit.bor(bit.lshift(g, 8), b))
+
+			local h = heightData:getPixel(x % iw, y % ih)
 			heights[idx] = h
 		end
 	end
@@ -205,7 +204,7 @@ function love.load()
 	hud = love.graphics.newImage("hud.png")
 	sky = love.image.newImageData("sky.png")
 	imageData = love.image.newImageData(PIXEL_WIDTH, PIXEL_HEIGHT)
-
+	bufferImage = love.graphics.newImage(imageData)
 
 	skyWidth = sky:getWidth()
 	skyHeight = sky:getHeight()
@@ -312,8 +311,7 @@ function love.draw()
 	-- alternate scanlines each frame
 	interlace = 1 - interlace
 
-	if not bufferImage then bufferImage = love.graphics.newImage(imageData)
-	else bufferImage:refresh() end
+	bufferImage:refresh()
 
 	love.graphics.draw(bufferImage, 0, 0, 0, PIXEL_SCALE)
 
